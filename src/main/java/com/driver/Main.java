37d8f4886e4bd74ea104ddf7dc79a355241e9c43
TarunVanshkar <tarunben33@gmail.com>
2023-01-13 21:58:02 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
-public class Main {
-  
+public class Main
+{
+
+    public static void main(String []args)
+    {
+        RWOnly f=new RWOnly();
+        f.setName("Error:");
+        f.getName();
+    }
 }
